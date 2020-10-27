2aec056ef792a9ba93d690026b5774eb82d78ea7
Cleiton Jorge <cleitonjorge@MacBook-Pro-de-Cleiton.local>
2020-10-27 06:38:55 -0300

    adicionado recursos de recepção de webhook e ociosidade

--- inactivity.js
+++ inactivity.js
@@ -15,155 +15,151 @@
  **/
 
 module.exports = function(RED) {
-  "use strict";
-  var cron = require("cron");
+    "use strict";
+    var cron = require("cron");
 
-  function InactivityNode(n) {
-      RED.nodes.createNode(this,n);
 
-      /* Handle legacy */
-      if(!Array.isArray(n.props)){
-          n.props = [];
-          n.props.push({
-              p:'payload',
-              v:n.payload,
-              vt:n.payloadType
-          });
-          n.props.push({
-              p:'topic',
-              v:n.topic,
-              vt:'str'
-          });
-      } else {
-          for (var i=0,l=n.props.length; i<l; i++) {
-              if (n.props[i].p === 'payload' && !n.props[i].hasOwnProperty('v')) {
-                  n.props[i].v = n.payload;
-                  n.props[i].vt = n.payloadType;
-              } else if (n.props[i].p === 'topic' && n.props[i].vt === 'str' && !n.props[i].hasOwnProperty('v')) {
-                  n.props[i].v = n.topic;
-              }
-          }
+    function msgInactivity(node, msg){
+        var userContext = node.context().flow;
+        var keys = userContext.keys();
+    
+        var today = new Date();
+        
+        var minutes;
+        var dados;
+        
+        
+        for(var i=keys.length-1;i>=0;i--){
+            if(keys[i].indexOf("user-")!= -1)
+            {
+                dados = userContext.get(keys[i]);
+                minutes = (parseInt((Math.abs(dados.lasttime) - today.getTime()) / (1000 * 60) % 60)*-1);
+                
+                if(minutes >= 1)
+                {
+                    if(!dados.count)
+                        dados.count = 0
+                        
+                    dados.count++;
+                    
+                    if(dados.count >= node.attempts)
+                    {
+                        userContext.set(keys[i], undefined);
+                        node.send([null, {"payload":{"appUser":{"_id":dados.id},"msg":msg}}])
+                        return;
+                    }
+                    node.send([{"payload":{"appUser":{"_id":dados.id}, "msg":msg}}]);
+                }
+            }
+        }
       }
 
-      this.props = n.props;
-      this.repeat = n.repeat;
-      this.crontab = n.crontab;
-      this.once = n.once;
-      this.onceDelay = (n.onceDelay || 0.1) * 1000;
-      this.interval_id = null;
-      this.cronjob = null;
-      var node = this;
+    function InactivityNode(n) {
+        RED.nodes.createNode(this,n);
+        this.attempts = n.attempts;
+        this.payload = n.payload;
+        this.payloadType = n.payloadType;
+        this.repeat = n.repeat;
+        this.crontab = n.crontab;
+        this.once = n.once;
+        this.onceDelay = (n.onceDelay || 0.1) * 1000;
+        this.interval_id = null;
+        this.cronjob = null;
+        var node = this;
 
-      node.props.forEach(function (prop) {
-          if (prop.vt === "jsonata") {
-              try {
-                  var val = prop.v ? prop.v : "";
-                  prop.exp = RED.util.prepareJSONataExpression(val, node);
-              }
-              catch (err) {
-                  node.error(RED._("inject.errors.invalid-expr", {error:err.message}));
-                  prop.exp = null;
-              }
-          }
-      });
+        if (node.repeat > 2147483) {
+            node.error(RED._("inject.errors.toolong", this));
+            delete node.repeat;
+        }
 
-      if (node.repeat > 2147483) {
-          node.error(RED._("inject.errors.toolong", this));
-          delete node.repeat;
-      }
-
-      node.repeaterSetup = function () {
+        node.repeaterSetup = function () {
           if (this.repeat && !isNaN(this.repeat) && this.repeat > 0) {
-              this.repeat = this.repeat * 1000;
-              if (RED.settings.verbose) {
-                  this.log(RED._("inject.repeat", this));
-              }
-              this.interval_id = setInterval(function() {
-                  node.emit("input", {});
-              }, this.repeat);
+            this.repeat = this.repeat * 1000;
+            if (RED.settings.verbose) {
+              this.log(RED._("inject.repeat", this));
+            }
+            this.interval_id = setInterval(function() {
+              node.emit("input", {});
+            }, this.repeat);
           } else if (this.crontab) {
-              if (RED.settings.verbose) {
-                  this.log(RED._("inject.crontab", this));
-              }
-              this.cronjob = new cron.CronJob(this.crontab, function() { node.emit("input", {}); }, null, true);
+            if (RED.settings.verbose) {
+              this.log(RED._("inject.crontab", this));
+            }
+            this.cronjob = new cron.CronJob(this.crontab, function() { node.emit("input", {}); }, null, true);
           }
-      };
+        };
 
-      if (this.once) {
-          this.onceTimeout = setTimeout( function() {
+        if (this.once) {
+            this.onceTimeout = setTimeout( function() {
               node.emit("input",{});
               node.repeaterSetup();
-          }, this.onceDelay);
-      } else {
+            }, this.onceDelay);
+        } else {
           node.repeaterSetup();
-      }
+        }
 
-      this.on("input", function(msg, send, done) {
-          var errors = [];
+        this.on("input",function(msg) {
+            msg.attempts = this.attempts;
+            if (this.payloadType !== 'flow' && this.payloadType !== 'global') {
+                try {
+                    if ( (this.payloadType == null && this.payload === "") || this.payloadType === "date") {
+                        msg.payload = Date.now();
+                    } else if (this.payloadType == null) {
+                        msg.payload = this.payload;
+                    } else if (this.payloadType === 'none') {
+                        msg.payload = "";
+                    } else {
+                        msg.payload = RED.util.evaluateNodeProperty(this.payload,this.payloadType,this,msg);
+                    }
+                    msgInactivity(node,msg);
+                    //this.send(msg);
+                    msg = null;
+                } catch(err) {
+                    this.error(err,msg);
+                }
+            } else {
+                RED.util.evaluateNodeProperty(this.payload,this.payloadType,this,msg, function(err,res) {
+                    if (err) {
+                        node.error(err,msg);
+                    } else {
+                        msg.payload = res;
+                        msgInactivity(node,msg);
+                        //node.send(msg);
+                    }
 
-          this.props.forEach(p => {
-              var property = p.p;
-              var value = p.v ? p.v : '';
-              var valueType = p.vt ? p.vt : 'str';
+                });
+            }
+        });
+    }
 
-              if (!property) return;
+    RED.nodes.registerType("Inactivity",InactivityNode);
 
-              if (valueType === "jsonata") {
-                  if (p.exp) {
-                      try {
-                          var val = RED.util.evaluateJSONataExpression(p.exp, msg);
-                          RED.util.setMessageProperty(msg, property, val, true);
-                      }
-                      catch  (err) {
-                          errors.push(err.message);
-                      }
-                  }
-                  return;
-              }
-              try {
-                  RED.util.setMessageProperty(msg,property,RED.util.evaluateNodeProperty(value, valueType, this, msg),true);
-              } catch (err) {
-                  errors.push(err.toString());
-              }
-          });
+    InactivityNode.prototype.close = function() {
+        if (this.onceTimeout) {
+            clearTimeout(this.onceTimeout);
+        }
+        if (this.interval_id != null) {
+            clearInterval(this.interval_id);
+            if (RED.settings.verbose) { this.log(RED._("inject.stopped")); }
+        } else if (this.cronjob != null) {
+            this.cronjob.stop();
+            if (RED.settings.verbose) { this.log(RED._("inject.stopped")); }
+            delete this.cronjob;
+        }
+    };
 
-          if (errors.length) {
-              done(errors.join('; '));
-          } else {
-              send(msg);
-              done();
-          }
-      });
-  }
-
-  RED.nodes.registerType("Inactivity", InactivityNode);
-
-  InactivityNode.prototype.close = function() {
-      if (this.onceTimeout) {
-          clearTimeout(this.onceTimeout);
-      }
-      if (this.interval_id != null) {
-          clearInterval(this.interval_id);
-          if (RED.settings.verbose) { this.log(RED._("inject.stopped")); }
-      } else if (this.cronjob != null) {
-          this.cronjob.stop();
-          if (RED.settings.verbose) { this.log(RED._("inject.stopped")); }
-          delete this.cronjob;
-      }
-  };
-
-  RED.httpAdmin.post("/inactivity/:id", RED.auth.needsPermission("inactivity.write"), function(req,res) {
-      var node = RED.nodes.getNode(req.params.id);
-      if (node != null) {
-          try {
-              node.receive();
-              res.sendStatus(200);
-          } catch(err) {
-              res.sendStatus(500);
-              node.error(RED._("inject.failed",{error:err.toString()}));
-          }
-      } else {
-          res.sendStatus(404);
-      }
-  });
+    RED.httpAdmin.post("/Inactivity/:id", RED.auth.needsPermission("inject.write"), function(req,res) {
+        var node = RED.nodes.getNode(req.params.id);
+        if (node != null) {
+            try {
+                node.receive();
+                res.sendStatus(200);
+            } catch(err) {
+                res.sendStatus(500);
+                node.error(RED._("inject.failed",{error:err.toString()}));
+            }
+        } else {
+            res.sendStatus(404);
+        }
+    });
 }
